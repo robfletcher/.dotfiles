26a4254d67848386a218523c3e3ba61b78cb65d2
Rob Fletcher <robert.w.fletcher@gmail.com>
2024-08-09 15:05:21 -0700

    nvm and pyenv

---
+++ workspaces/work/symlinks/nvm/.nvm.zsh
@@ -0,0 +1,4 @@
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[[ -d "$NVM_DIR" ]] || mkdir -p "$NVM_DIR"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
