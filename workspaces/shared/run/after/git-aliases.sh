6406c637fe809636b393fe0c5acdc70c4e29b1d6
Rob Fletcher <robert.w.fletcher@gmail.com>
2024-09-10 13:00:34 -0700

    git gone

--- workspaces/shared/run/after/git-aliases.sh
+++ workspaces/shared/run/after/git-aliases.sh
@@ -2,3 +2,4 @@
 
 git config --global --replace-all alias.amend 'commit --amend --no-edit'
 git config --global --replace-all alias.please 'push --force-with-lease'
+git config --global --replace-all alias.gone "! git fetch -p && git for-each-ref --format '%(refname:short) %(upstream:track)' | awk '\$2 == \"[gone]\" {print \$1}' | xargs -r git branch -D"
