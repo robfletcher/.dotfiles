ea6ddf12770997b6948421119dedaa345d055438
Rob Fletcher <robert.w.fletcher@gmail.com>
2025-03-12 13:28:53 -0700

    git config & k8s tools

--- workspaces/shared/run/after/git-config.zsh
+++ workspaces/shared/run/after/git-config.zsh
@@ -2,13 +2,14 @@
 
 typeset -A git_config
 git_config=(
-	core.precomposeunicode true # handles MacOS filenames in a manner more compatible with Win/Linux
-	pull.rebase true            # always rebase rather than merge when pulling
-	push.default current        # if no remote ref is specified push to a branch with the same name as the current local branch
-	rebase.autoStash true       # automatically stash when rebasing if there are uncomitted changes
-	rerere.enabled true         # something to do with conflict handling
-	status.branch true          # display branch header in status
-	status.short true           # default to short status, --no-short to disable
+	core.precomposeunicode true     # handles MacOS filenames in a manner more compatible with Win/Linux
+	pull.rebase true                # always rebase rather than merge when pulling
+	push.default current            # if no remote ref is specified push to a branch with the same name as the current local branch
+	rebase.autoStash true           # automatically stash when rebasing if there are uncomitted changes
+	rerere.enabled true             # something to do with conflict handling
+	status.branch true              # display branch header in status
+	status.short true               # default to short status, --no-short to disable
+	advice.skippedCherryPicks false # don't nag about skipped commits when rebasing
 
 	alias.amend 'commit --amend --no-edit'
 	alias.please 'push --force-with-lease'
